e43eda7f2de28d3f22c067058f79f73a505a231c
tolikavr <anatoliy.avr@gmail.com>
2023-03-12 17:58:54 +0300

    done GameFinishedFragmentDataBinding

--- presentation/src/main/java/com/example/composition_v2/adapter/BindingAdapter.kt
+++ presentation/src/main/java/com/example/composition_v2/adapter/BindingAdapter.kt
@@ -1,13 +1,42 @@
 package com.example.composition_v2.adapter
 
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.composition_v2.R
 import com.example.domain.entity.GameResult
 
-@BindingAdapter("bindScorePercentage")
-fun bindScorePercentage(textView: TextView, gameResult: GameResult) {
+@BindingAdapter("resultEmoji")
+fun bindResultEmoji(imageView: ImageView, winner: Boolean) {
+    imageView.setImageResource(if (winner) R.drawable.ic_smile else R.drawable.ic_sad)
+}
 
+@BindingAdapter("requireAnswers")
+fun bindRequireAnswers(textView: TextView, count: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.required_score),
+        count
+    )
+}
+
+@BindingAdapter("score")
+fun bindScore(textView: TextView, score: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.score_answers),
+        score
+    )
+}
+
+@BindingAdapter("requiredPercentage")
+fun bindRequiredPercentage(textView: TextView, percentage: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.required_percentage),
+        percentage
+    )
+}
+
+@BindingAdapter("scorePercentage")
+fun bindScorePercentage(textView: TextView, gameResult: GameResult) {
     textView.text = String.format(
         textView.context.getString(R.string.score_percentage),
         getPercentOfRightAnswers(gameResult)
--- presentation/src/main/java/com/example/composition_v2/ui/GameFinishedFragment.kt
+++ presentation/src/main/java/com/example/composition_v2/ui/GameFinishedFragment.kt
@@ -25,27 +25,9 @@ class GameFinishedFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupClickListener()
-        bindView()
+        binding.gameResult = args.gameResult
     }
 
-    private fun bindView() {
-        with(binding) {
-            gameResult = args.gameResult
-//            tvRequiredAnswers.text = getString(R.string.required_score, args.gameResult.gameSettings.minCountOfRightAnswers)
-//            tvScoreAnswers.text = getString(R.string.score_answers, args.gameResult.countOfRightAnswers)
-//            tvRequiredPercentage.text = getString(R.string.required_percentage, args.gameResult.gameSettings.minPercentOfRightAnswers)
-//            tvScorePercentage.text = getString(R.string.score_percentage, getPercentOfRightAnswer())
-        }
-    }
-
-//    private fun getPercentOfRightAnswer() = with(args.gameResult) {
-//        if (countOfQuestions == 0) 0
-//        else ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
-//    }
-
-//    private fun getSmileResId(): Int {
-//        return if (args.gameResult.winner) R.drawable.ic_smile else R.drawable.ic_sad
-//    }
 
     private fun setupClickListener() {
         binding.buttonRetry.setOnClickListener {
